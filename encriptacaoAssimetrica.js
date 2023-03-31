fa2cb9d926690b4ec60837435b1769bf9c9d08a9
barretodf <edsb1981@gmail.com>
2023-03-30 22:38:15 -0300

    Public Key e Private Key

---
+++ encriptacaoAssimetrica.js
@@ -0,0 +1,19 @@
+import { generateKeyPairSync } from 'crypto'
+
+const {privateKey, publicKey } = generateKeyPairSync('rsa',
+    {
+        modulusLength: 2048,
+
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem',
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem',
+        },
+    }
+)
+
+//console.log (publicKey)
+//console.log (privateKey)
