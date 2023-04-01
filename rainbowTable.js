fd78037806ea611a996266708b867d7a6e50dc24
barretodf <edsb1981@gmail.com>
2023-04-01 03:27:22 -0300

    Encontrando a senha com ataque de  rainbow

--- rainbowTable.js
+++ rainbowTable.js
@@ -6,7 +6,6 @@ function criaHash (dado, estrategia){
 
 const senhasComuns = ["senha", "123456", "senha123", "admin","senha123456","1234",
  "blink182","meuAniversario", "senha123456", "brasil", "102030"]
-//código omitido
 
 const rainbowTable = senhasComuns.map( senha => {
     return [ senha, criaHash(senha, "MD5")]
@@ -17,3 +16,11 @@ console.log(rainbowTable)
 const hashesVazadas = ["21232f297a57a5a743894a0e4a801fc3",
 "cedf5ab7b5c5852b3ed35d7dbe3c3835",
 "81dc9bdb52d04dc20036dbd8313ed055"]
+
+hashesVazadas.map(hashesVazada => {
+    rainbowTable.map( parGerado => {
+        if (hashesVazada === parGerado[1]){
+            console.log(`Senha encontrada: a hash ${hashesVazada} equivale à ${parGerado[0]}`)
+        }
+    })
+})
